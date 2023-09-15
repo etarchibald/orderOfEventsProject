652279664df059aef915ef0f28954648c4f5c143
etarchibald <ethanarchibald@Veronica.local>
2023-09-15 09:29:49 -0600

    Init Commit

---
+++ OrderOfEvents/OrderOfEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderOfEvents
+//
+//  Created by Ethan Archibald on 9/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
